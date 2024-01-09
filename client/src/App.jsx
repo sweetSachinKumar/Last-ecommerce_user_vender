d5c6782e1ea301bc80d033491aca7c9eb88a2ce2
sweetSachinKumar <k6617626@gmail.com>
2024-01-10 05:18:20 +0530

    first commit
    just simple change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1 className="text-3xl font-bold underline text-gray-700/80">
     Hello world! this is sachin
   </h1>
+  <p className='text-xl leading-tight tracking-tighter text-slate-700 font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, quis! Repellat porro illum soluta nemo, quidem, magni hic inventore non nulla mollitia cupiditate impedit voluptates corrupti possimus voluptatibus rerum amet.</p>
+  </>
   )
 }
 
