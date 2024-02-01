d90236072275182b1146e9af2b4106174fd6231a
sweetSachinKumar <k6617626@gmail.com>
2024-02-01 10:32:38 +0530

    fffss
    sss

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:5173',],
+    origin: ['https://last-ecommerce-user-vender.vercel.app/',],
     credentials: true
   }));
 
