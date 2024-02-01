fc1a90a1f29fdb890e0d659eaddd71324f020d72
sweetSachinKumar <110831232+sweetSachinKumar@users.noreply.github.com>
2024-02-01 10:54:59 +0530

    Update app.js
     changed

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://last-ecommerce-user-vender.vercel.app/',],
+    origin: ['https://last-ecommerce-user-vender.vercel.app',],
     credentials: true
   }));
 
